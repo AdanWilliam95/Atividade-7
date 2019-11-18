b60168df3f07a373d37bb7b9ada0c20d9e547625
Adan William Oliveira Santos <awos@aluno.ifnmg.edu.br>
2019-11-17 21:57:41 -0300

    Atualizando

---
+++ src/AssistenteTec.java
@@ -0,0 +1,16 @@
+  
+public class AssistenteTec extends Funcionario {
+    private double bonus;
+    
+    public AssistenteTec(String n, String m, double b){
+        super(n,m);
+        this.setBonus(b);
+    }
+    public void setBonus(double b){
+        this.bonus = b;
+    }
+    
+    public double getBonus(){
+        return this.bonus;
+    }
+}
